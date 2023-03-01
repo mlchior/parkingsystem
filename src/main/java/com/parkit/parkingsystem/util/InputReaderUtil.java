4952c2be35d0d3b8cec62d40e58546014447a0ea
Melchior Crinier <66031896+mlchior@users.noreply.github.com>
2023-03-01 10:28:46 +0100

    soutenance

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class InputReaderUtil {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static final Scanner scan = new Scanner(System.in);
     private static final Logger logger = LogManager.getLogger("InputReaderUtil");
 
     public int readSelection() {
